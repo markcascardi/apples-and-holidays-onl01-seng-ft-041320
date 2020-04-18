ba4b37e66c48d2be8efe3fa6d50915ee8abb13b2
Mark Cascardi <mcascardi@gmail.com>
2020-04-18 08:19:04 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,22 +1,4 @@
-require 'pry'
-holiday_hash = {
-  :winter => {
-    :christmas => ["Lights", "Wreath"],
-    :new_years => ["Party Hats"]
-  },
-  :summer => {
-    :fourth_of_july => ["Fireworks", "BBQ"]
-  },
-  :fall => {
-    :thanksgiving => ["Turkey"]
-  },
-  :spring => {
-    :memorial_day => ["BBQ"]
-  }
-}
-
 def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
   holiday_hash = {
     :winter => {
       :christmas => ["Lights", "Wreath"],
@@ -32,7 +14,6 @@ def second_supply_for_fourth_of_july(holiday_hash)
       :memorial_day => ["BBQ"]
     }
   }
-  # return the second element in the 4th of July array
 end
 
 def second_supply_for_fourth_of_july(holiday_supplies)
@@ -44,7 +25,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash[:winter][:new_years] << supply
 end
 
-
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day] = supply
 end
@@ -52,12 +32,10 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
- end
+end
 
 def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values.flatten
-  # return an array of all of the supplies that are used in the winter season
-
 end
 
 def all_supplies_in_holidays(holiday_hash)
